b6d66138efbf43454ec7de41672f4cccc98ea024
alfredo pezzotti <alfredo.pezzotti@yahoo.com>
2020-11-28 17:41:34 +0100

    Implements the main layout of the GUI

--- debug.sh
+++ debug.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-gcc -g -o sudoku-solver sudoku.c engine.c main.c
+gcc -g -o sudoku-debug sudoku.c engine.c main.c
 
 
--- sudoku-solver.py
+++ sudoku-solver.py
@@ -7,5 +7,48 @@
 """
 
 import tkinter as tk
+from tkinter import ttk
+
+
+# definitions:
+def Open():
+    # define code here
+    pass
+
+
+#
+root = tk.Tk()
+#root.geometry("400x400")
+frame = tk.Frame(root)
+frame.pack()
+
+# menu bar:
+mainMenu = tk.Menu(frame)
+mainMenu.add_command(label="Open", command=Open)
+root.config(menu = mainMenu)
+
+topFrame = tk.Frame(root)
+#topFrame.config(bg="#FFFFFF")
+topFrame.pack(side="top")
+
+bottomFrame = tk.Frame(root)
+bottomFrame.pack(side="bottom")
+
+# create grid
+for r in range (11):
+    for c in range (11):
+        if (c != 3) and (c != 7) and (r != 3) and (r != 7):
+            tk.Entry(topFrame, width=2).grid(row=r, column=c)
+        elif ((c == 3) or (c == 7)) and ((r != 3) and (r != 7)):
+            tk.Label(topFrame, text="|", borderwidth=2).grid(row=r, column=c)
+        else:
+            tk.Label(topFrame, text='-', borderwidth=1).grid(row=r, column=c)
+
+# create solve button:
+solveBtn = tk.Button(bottomFrame, text="solve!")
+solveBtn.pack()
+
+# start of program:
+root.mainloop()
 
 #EOF
